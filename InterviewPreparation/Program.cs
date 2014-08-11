3f1ffc15d22ab12c4be20e36466923788e2a7015
arun.dodla <arun.dodla@hotmail.com>
2014-08-10 20:51:18 -0700

    Added Euclid's Algorithm

---
+++ InterviewPreparation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int p = 20 % 10;
+            Console.WriteLine(GreatestCommonDivisor(10,20));
+            Console.ReadLine();
+        }
+
+
+
+
+
+        public static  int GreatestCommonDivisor(int p, int q)
+        {
+            if (q == 0)
+                return p;
+            int r = p % q;
+            return GreatestCommonDivisor(q, r);
+
+        }
+    }
+}
